e727dd52277bb21c7cd93253de056ffa4616a295
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-05-04 22:00:00 -0500

    Se finalizo la parte basica de express

---
+++ proyecto/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const app = express();
+/* ./ nos indica que no es un modulo principal como por ejemplo express */
+const {infoCursos} = require('./cursos.js');
+
