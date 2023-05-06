4e7b7e17dc57ffa0fbcab8af4fa18b8dbd49803c
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-05-05 21:00:09 -0500

    Se agregaron ejemplos de Parametros URL

--- proyecto/app.js
+++ proyecto/app.js
@@ -17,6 +17,40 @@ app.get('/api/cursos',(req,res) =>{
 app.get('/api/cursos/programacion',(req,res) =>{
     res.send(infoCursos.programacion);
 });
+/*Parametros URL para poder manejar rutas de mejor forma 
+en vez de escribir app.get(/api/cursos/programacion/python o javascript) 
+usamos /api/cursos/programacion/:lenguaje */
+app.get('/api/cursos/programacion/:lenguaje',(req,res) =>{
+    const lenguaje = req.params.lenguaje;
+    const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje);
+    if(resultados.length === 0){
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje}`)
+    }
+    res.send(JSON.stringify(resultados));
+});
+
+app.get('/api/cursos/matematicas/:tema',(req,res) =>{
+    const tema = req.params.tema;
+    const  resultados = infoCursos.matematicas.filter(curso => curso.tema === tema);
+    if(resultados.length === 0){
+        return res.status(404).send(`No se encontraron cursos de ${tema}`)
+    }
+    res.send(JSON.stringify(resultados));
+});
+
+app.get('/api/cursos/programacion/:lenguaje/:nivel',(req,res)=>{
+    const lenguaje = req.params.lenguaje;
+    const nivel = req.params.nivel;
+
+    const resultados = infoCursos.programacion.filter(curso =>curso.lenguaje === lenguaje);
+
+    if(resultados.length === 0){
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+    }
+
+    res.send(JSON.stringify(resultados));
+
+});
 
 app.get('/api/cursos/matematicas',(req,res)=>{
     res.send(infoCursos.matematicas);
